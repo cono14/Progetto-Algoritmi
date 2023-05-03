91966b601884a60f714df641c1eefd91213c6d42
cono14 <112408901+cono14@users.noreply.github.com>
2023-05-03 10:30:07 +0200

    Add files via upload

--- LXI.java
+++ LXI.java
@@ -10,6 +10,7 @@ import java.util.TreeSet;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,16 +23,24 @@ public class LXI implements CXPlayer {
     private CXGameState yourWin;
     private int TIMEOUT;
     private long START;
-    private int[][] transposition_table;
-    private static int SIZE = 1024 * 1024 * 6;
     private long[][][] zobrist;
-    private int TIMEOUT_VALUE = 999999;
-    private static int ERROR = Integer.MAX_VALUE;
-    private static int flag_exact = 0;
-    private static int flag_lower = -1;
-    private static int flag_upper = 1;
     private long hash_value;
-    int[][] killerMoves;
+    private int size = 2 ^ 24;
+    private Entry[] transpositionTable = new Entry[size]; // transposition table structure
+    private int hashfEXACT = 0; // hash flag exact
+    private int hashfALPHA = 1; // hash flag lower
+    private int hashfBETA = 2; // hash flag upper
+    private int valUNKOWN = Integer.MIN_VALUE - 1;
+    private int timeout_v = 10000;
+    private boolean firstPlayer;
+    private int[] HistoryTable;
+
+    private static class Entry {
+        long key;
+        int depth;
+        int score;
+        int flag;
+    } // structure of a transposition table entry
 
     public LXI() {
     }
@@ -42,20 +51,18 @@ public class LXI implements CXPlayer {
         myWin = first ? CXGameState.WINP1 : CXGameState.WINP2;
         yourWin = first ? CXGameState.WINP2 : CXGameState.WINP1;
         TIMEOUT = timeout_in_secs;
+        firstPlayer = first ? true : false;
+
         zobrist = new long[M][N][2];
-        transposition_table = new int[SIZE][];
-        hash_value = 0;
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                zobrist[i][j][0] = rand.nextLong();
-                zobrist[i][j][1] = rand.nextLong();
+                for (int k = 0; k < 2; k++) {
+                    zobrist[i][j][k] = rand.nextLong();
+                }
             }
         }
-        killerMoves = new int[N + 1][2];
-        for (int i = 0; i < N + 1; i++) {
-            killerMoves[i][0] = -1;
-            killerMoves[i][1] = -1;
-        }
+        hash_value = rand.nextLong();
+        HistoryTable = new int[N];
     }
 
     private CXGameState doMove(CXBoard B, int y) {
@@ -65,60 +72,37 @@ public class LXI implements CXPlayer {
     }
 
     private void undoMove(CXBoard B) {
-        CXCell c = B.getMarkedCells()[B.getMarkedCells().length - 1];
-        hash_value ^= zobrist[c.i][c.j][B.currentPlayer() == 1 ? 0 : 1]; // la mossa è del player precedente
+        CXCell cell = B.getMarkedCells()[B.getMarkedCells().length - 1];
+        hash_value ^= zobrist[cell.i][cell.j][B.currentPlayer() == 1 ? 0 : 1]; // la mossa è del player precedente
         B.unmarkColumn();
     }
 
     private int Index() {
-        return Math.abs((int) (hash_value % SIZE));
+        return Math.abs((int) (hash_value % size));
     }
 
-    private void put(int depth, int bestMove, int value, int flag_type) {
-        int firstHash = (int) (hash_value >> 32), secondHash = (int) hash_value;
-        if (transposition_table[Index()] == null) { // avoiding collisions
-            transposition_table[Index()] = new int[] { firstHash, secondHash, depth, bestMove, value, flag_type };
-        } else {
-            int i = 1;
-            while ((Index() + i < SIZE) && transposition_table[Index() + i] != null) {
-                i++;
-            }
-            if (Index() + i >= SIZE) { // devo sovrascrivere un dato che avevo salvato
-                // lo faccio solo se: 1- ha profondità maggiore 2- se non ha profondità maggiore
-                // ma ha un flag migliore
-                int[] prec_element = transposition_table[Index()];
-                if (depth >= prec_element[2]) {
-                    transposition_table[Index()] = new int[] { firstHash, secondHash, depth, bestMove, value,
-                            flag_type };
-                } else if (flag_type != flag_exact && prec_element[5] == flag_exact) {
-                    transposition_table[Index()] = new int[] { firstHash, secondHash, depth, bestMove, value,
-                            flag_type };
+    private int get(int searchdepth, int alpha, int beta) {
+        Entry t_element = transpositionTable[Index()];
+        if (t_element != null && t_element.key == hash_value) {
+            if (t_element.depth >= searchdepth) {
+                if (t_element.flag == hashfEXACT) {
+                    return t_element.score;
+                } else if (t_element.flag == hashfALPHA && t_element.score <= alpha) {
+                    return alpha;
+                } else if (t_element.flag == hashfBETA && t_element.score >= beta) {
+                    return beta;
                 }
-            } else { // occupo la prima posizione libera
-                transposition_table[Index() + i] = new int[] { firstHash, secondHash, depth, bestMove, value,
-                        flag_type };
             }
-
         }
-
+        return valUNKOWN;
     }
 
-    private int get(int depth, int alpha, int beta) {
-        int[] t_element = transposition_table[Index()];
-        int firstHash = (int) (hash_value >> 32), secondHash = (int) hash_value;
-        if (t_element != null && firstHash == t_element[0] && secondHash == t_element[1] && (t_element[2] >= depth)) {
-            if (t_element[5] == flag_exact) {
-                return t_element[4];
-            }
-            if (t_element[5] == flag_lower && t_element[4] <= alpha) {
-                return alpha;
-            }
-            if (t_element[5] == flag_upper && t_element[4] >= beta) {
-                return beta;
-            }
-
-        }
-        return ERROR;
+    private void put(int depth, int val, int hash_flag) {
+        transpositionTable[Index()] = new Entry();
+        transpositionTable[Index()].key = hash_value;
+        transpositionTable[Index()].depth = depth;
+        transpositionTable[Index()].flag = hash_flag;
+        transpositionTable[Index()].score = val;
     }
 
     public static int getX(CXBoard B, int y) {
@@ -135,7 +119,6 @@ public class LXI implements CXPlayer {
     // usare un array per salvare l'altezza raggiunta in ogni colonna
 
     public int eval(CXBoard board, int col) {
-
         int best = 0;
         int secondbest = 0;
         int last_row = getX(board, col);
@@ -209,121 +192,111 @@ public class LXI implements CXPlayer {
         }
     }
 
-    private int[] getOrderedColumns(CXBoard B, int depth) {
-        int[] orderedColumns = new int[B.getAvailableColumns().length + 3];
-        int j = 0;
-
-        if (depth > 0) {
-            for (int i = 0; i < 2; i++) {
-                int killerMove = killerMoves[depth][i];
-                if (killerMove != -1 && !B.fullColumn(killerMove)) {
-                    orderedColumns[j] = killerMove;
-                    j++;
-                }
-            }
-        } // controlliamo se alla profondità precedente abbiamo trovato delle killer moves
-          // e aggiorniamo
-
-        for (int col : B.getAvailableColumns()) {
-            if (!Arrays.asList(orderedColumns).contains(col)) { // controlliamo di non aggiungere due volte le killer
-                                                                // moves
-                orderedColumns[j] = col;
-                j++;
-            }
+    private int[] HistoryHeuristic(CXBoard B) {
+        int width = B.getAvailableColumns().length;
+        int[] rating = new int[width];
+        Integer[] moves = B.getAvailableColumns();
+        for (int m = 0; m < width; m++) {
+            rating[m] = HistoryTable[moves[m]];
         }
-        return Arrays.copyOf(orderedColumns, j);
+
+        // Ordina l'array rating in ordine decrescente
+        Integer[] indexes = new Integer[width];
+        for (int i = 0; i < width; i++) {
+            indexes[i] = i;
+        }
+        Arrays.sort(indexes, Comparator.comparingInt((Integer i) -> rating[i]).reversed());
+
+        // Restituisci i movimenti ordinati in base al rating
+        int[] orderedMoves = new int[width];
+        for (int i = 0; i < width; i++) {
+            orderedMoves[i] = moves[indexes[i]];
+        }
+        return orderedMoves;
     }
 
     public int alphabeta(CXBoard board, int depth, int alpha, int beta, boolean maximizingPlayer) {
-        int value, valueKind = flag_exact;
-        int localBestMove = -1;
+        int value, valueKind = hashfEXACT;
+        int color = 1;
+        if (firstPlayer == false) {
+            color = -1;
+        }
+
         value = get(depth, alpha, beta);
-        if (value != ERROR) {
+        if (value != valUNKOWN) {
             return value;
         }
         if (checktime()) {
-            return TIMEOUT_VALUE;
+            return timeout_v;
         }
+
         if (depth == 0 || board.gameState() != CXGameState.OPEN) {
             if (board.gameState() != CXGameState.OPEN && board.gameState() == myWin) {
                 return Integer.MAX_VALUE; // win
             } else if (board.gameState() != CXGameState.OPEN && board.gameState() == yourWin) {
                 return Integer.MIN_VALUE; // loss
             } else {
-                value = eval(board, board.getLastMove().j);
-                put(depth, board.getLastMove().j, value, valueKind);
-                return value; // draw
-
+                value = eval(board, board.getLastMove().j) * color;
+                put(depth, value, hashfEXACT);
+                return value;// draw
             }
         }
         // Massimizzare la valutazione per il giocatore corrente
         if (maximizingPlayer) {
             int bestValue = Integer.MIN_VALUE;
-
-            for (int column : getOrderedColumns(board, depth)) {
+            for (int column : HistoryHeuristic(board)) {
                 // Effettuare la mossa sulla colonna selezionata
-
                 doMove(board, column);
 
                 // Calcolare il valore della mossa
                 value = alphabeta(board, depth - 1, alpha, beta, false);
+                HistoryTable[column] += value * value;
+                if (checktime()) {
+                    return timeout_v;
+                }
                 bestValue = Math.max(bestValue, value);
-
-                if (value > bestValue) {
-                    localBestMove = column;
-                    bestValue = value;
+                if (bestValue >= beta) {
+                    valueKind = hashfBETA;
+                } else if (bestValue <= alpha) {
+                    valueKind = hashfALPHA;
                 }
                 // Aggiornare il valore di alpha
-                if (bestValue >= beta) {
-                    valueKind = flag_upper;
-                    killerMoves[depth][1] = killerMoves[depth][0];
-                    killerMoves[depth][0] = column;
-                } else if (bestValue <= alpha) {
-                    valueKind = flag_lower;
-                } else {
-                    valueKind = flag_exact;
-                }
                 alpha = Math.max(alpha, bestValue);
 
                 // Annullare la mossa effettuata sulla colonna selezionata
-                // undoMove(board);
-                board.unmarkColumn();
+                undoMove(board);
 
                 // Verificare se si può tagliare il ramo
                 if (beta <= alpha) {
                     break;
                 }
             }
-            put(depth, localBestMove, bestValue, valueKind);
+            put(depth, bestValue, valueKind);
             return bestValue;
         }
-
         // Minimizzare la valutazione per l'avversario
         else {
             int bestValue = Integer.MAX_VALUE;
-
-            for (int column : getOrderedColumns(board, depth)) {
+            for (int column : HistoryHeuristic(board)) {
                 // Effettuare la mossa sulla colonna selezionata dall'avversario
                 doMove(board, column);
 
                 // Calcolare il valore della mossa
                 value = alphabeta(board, depth - 1, alpha, beta, true);
+                HistoryTable[column] += value * value;
+                if (checktime()) {
+                    return timeout_v;
+                }
                 if (value < bestValue) {
-                    localBestMove = column;
                     bestValue = value;
-
+                }
+                if (bestValue <= alpha) {
+                    valueKind = hashfALPHA;
+                } else if (bestValue >= beta) {
+                    valueKind = hashfBETA;
                 }
 
                 // Aggiornare il valore di beta
-                if (bestValue >= beta) {
-                    valueKind = flag_upper;
-                } else if (bestValue <= alpha) {
-                    valueKind = flag_lower;
-                    killerMoves[depth][1] = killerMoves[depth][0];
-                    killerMoves[depth][0] = column;
-                } else {
-                    valueKind = flag_exact;
-                }
                 beta = Math.min(beta, bestValue);
 
                 // Annullare la mossa effettuata sulla colonna selezionata
@@ -334,9 +307,32 @@ public class LXI implements CXPlayer {
                     break;
                 }
             }
-            put(depth, localBestMove, bestValue, valueKind);
+            put(depth, bestValue, valueKind);
             return bestValue;
         }
+    }
+
+    private int alphabetacol(CXBoard B, int depth) {
+        int value;
+        int bestValue = Integer.MIN_VALUE;
+        int bestMove = -1;
+        for (int i : HistoryHeuristic(B)) {
+            doMove(B, i);
+            if (B.gameState() == myWin) {
+                System.out.println("Serve a qualcosaID"); // serve a qualcosa
+                return i;
+            }
+            value = alphabeta(B, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+            undoMove(B);
+            if (value == timeout_v) {
+                break;
+            }
+            if (value > bestValue) {
+                bestValue = value;
+                bestMove = i;
+            }
+        }
+        return bestMove;
     }
 
     public int center(CXBoard B) {
@@ -349,20 +345,19 @@ public class LXI implements CXPlayer {
 
     public int LosingColumn(CXBoard B) {
         int col = B.getAvailableColumns()[0];
-        // doMove(B, col);
         doMove(B, col);
         for (int k = 1; k < B.getAvailableColumns().length; k++) {
             if (k != col && B.gameState() == CXGameState.OPEN) { // per evitare l'errore 'Game'
                 int col_2 = B.getAvailableColumns()[k];
                 doMove(B, col_2);
                 if (B.gameState() == yourWin) {
-                    System.out.println("Sconfitta evitata");
+                    System.out.println("Sconfitta evitataID");
                     undoMove(B);// UNDO YOUR MOVE
-                    undoMove(B); // undo my move
+                    undoMove(B);// undo my move
                     doMove(B, col_2); // steal your move
                     return col_2;
                 } else {
-                    undoMove(B);
+                    undoMove(B); //
                 }
             }
         }
@@ -370,13 +365,29 @@ public class LXI implements CXPlayer {
         return -1;
     }
 
+    private int iterativeDeepening(int max_depth, CXBoard B) {
+        int current_depth_limit = 3;
+        Integer[] L = B.getAvailableColumns();
+        int safe = L[rand.nextInt(L.length)]; // Save a random column
+        int bestMove = -1;
+        while (!checktime() && current_depth_limit <= max_depth && B.gameState() == CXGameState.OPEN) {
+            int it_col = alphabetacol(B, current_depth_limit);
+            if (it_col == -1) {
+                break;
+            }
+            bestMove = it_col;
+            current_depth_limit++;
+        }
+        if (bestMove == -1) {
+            System.out.println("Failed");
+            return safe;
+        }
+        System.out.println("Good Job");
+        return bestMove;
+    }
+
     public int selectColumn(CXBoard B) {
         START = System.currentTimeMillis(); // Save starting time
-
-        Integer[] L = B.getAvailableColumns();
-        int save = L[rand.nextInt(L.length)]; // Save a random column
-        int bestValue = -1;
-        int best_Move = -1;
         int col = center(B);
         if (col != -1)
             return col;
@@ -385,51 +396,20 @@ public class LXI implements CXPlayer {
         if (possible_lose != -1) {
             return possible_lose;
         }
-
-        int current_depth_limit = 3;
-        int max_depth = B.getAvailableColumns().length;
-        while (current_depth_limit <= max_depth && !checktime()) {
-            for (int i : L) {
-                doMove(B, i);
-
-                if (B.gameState() == myWin) {
-                    System.out.println("Serve a qualcosa"); // serve a qualcosa
-                    return i;
-                }
-
-                int value = alphabeta(B, current_depth_limit, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
-                undoMove(B);
-                if (value > bestValue) {
-                    bestValue = value;
-                    best_Move = i;
-                }
-            }
-            max_depth = B.getAvailableColumns().length;
-            current_depth_limit++;
-        }
-
-        // Select a random column if no valid moves are available
-        if (best_Move == -1) {
-            System.err.println("No");
-
-            return save;
-        }
-        System.out.println("At least one valid move calculated");
-
-        return best_Move;
+        int max_depth = (B.M * B.N) - B.getMarkedCells().length;
+        int bestMove = iterativeDeepening(max_depth, B);
+        return bestMove;
     }
 
-    /**
-     * Check if we can block adversary's victory
-     *
-     * Returns a blocking column if there is one, otherwise a random one
-     */
-
     private boolean checktime() {
-        return (System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0);
+        if ((System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0)) {
+            return true;
+        }
+        return false;
     }
 
     public String playerName() {
-        return "LXI";
+        return "LXImo";
     }
+
 }
